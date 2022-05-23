7fc5570dca9f1915709bed00c9b18ae164873169
efren <efrenbahen@gmail.com>
2022-05-23 14:43:08 -0500

    add negative number button functionality

--- calculator/src/app/components/calculator/calculator.component.ts
+++ calculator/src/app/components/calculator/calculator.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  result: string = "0";
+  result: string = "";
   currentEq: string = "";
 
   constructor() { }
@@ -22,13 +22,9 @@ export class CalculatorComponent implements OnInit {
       return;
     }
     
+    //no double decimals
     let lastInput = this.currentEq[this.currentEq.length - 1];
-    let operations = ["+", "-", "*", "/"];
-
-    if(number == "0" && operations.includes(lastInput)) {
-      return;
-    }
-
+   
     if(lastInput == "." && number == ".") {
       return;
     }
@@ -41,6 +37,7 @@ export class CalculatorComponent implements OnInit {
     let lastInput = this.currentEq[this.currentEq.length - 1];
     let operations = ["+", "-", "*", "/"];
 
+    //no double operations
     if(operations.includes(lastInput)) {
       return;
     }
@@ -52,28 +49,55 @@ export class CalculatorComponent implements OnInit {
   valueChange(){
     let operations = ["+", "-", "*", "/"];
 
-    if(operations.includes(this.currentEq) || this.currentEq == "") {
-      return;
+    let hasOperation = false;
+
+    for(let i = 0; i < this.currentEq.length; i++) {
+      if(operations.includes(this.currentEq[i])) {
+        hasOperation = true;
+      }
     }
 
-    if(this.currentEq[0] == "-"){
-      this.currentEq = this.currentEq.slice(1);
-    } else {
-      this.currentEq = "-" + this.currentEq;
+    if(hasOperation) {
+      //add negative sign to last number
+      let lastOperationIdx = -1;
+
+      for(let i = this.currentEq.length - 1; i >= 0; i--) {
+        if(this.currentEq[i] == "-"){
+          return;
+        } else if(operations.includes(this.currentEq[i])) {
+          lastOperationIdx = i;
+          break;
+        }
+      }
+
+      this.currentEq = this.currentEq.slice(0, lastOperationIdx + 1) + "-" + this.currentEq.slice(lastOperationIdx + 1);
+    
+    } else if(!hasOperation && this.currentEq != "") {
+      //add negative sign to first number
+      if(this.currentEq[0] == "-"){
+        this.currentEq = this.currentEq.slice(1);
+      } else {
+        this.currentEq = "-" + this.currentEq;
+      }
+    
     }
+
   }
 
+  //delete last character
   backup() {
     if (this.currentEq !="" ) {
       this.currentEq=this.currentEq.slice(0, this.currentEq.length-1)
     }
   }
  
+  //delete all characters
   clear() {
     this.result = '';
     this.currentEq = '';
   }
 
+  //calculate equation
   calcEq() {
     let problem = this.currentEq;
 
@@ -85,9 +109,9 @@ export class CalculatorComponent implements OnInit {
     }
  
     this.result = eval(problem);
-    console.log(this.result);
   }
 
+  //return result
   solveEq(){
     this.calcEq();
     this.currentEq = this.result;
