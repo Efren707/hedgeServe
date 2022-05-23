010dff9aaf538b4fd10a7a87852b91411f7bcf25
efren <efrenbahen@gmail.com>
2022-05-23 12:35:18 -0500

    add calculator functionality

--- calculator/src/app/components/calculator/calculator.component.ts
+++ calculator/src/app/components/calculator/calculator.component.ts
@@ -7,11 +7,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  result: number = 0;
+  result: string = "0";
+  currentEq: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  numberPress(number: string) {
+
+    //no leading zeros
+    if (number == "0" && this.currentEq == "") {
+      return;
+    }
+    
+    let lastInput = this.currentEq[this.currentEq.length - 1];
+    let operations = ["+", "-", "*", "/"];
+
+    if(number == "0" && operations.includes(lastInput)) {
+      return;
+    }
+
+    if(lastInput == "." && number == ".") {
+      return;
+    }
+
+    this.currentEq += number;
+    this.calcEq();
+  }
+
+  operationPress(operation: string) {
+    let lastInput = this.currentEq[this.currentEq.length - 1];
+    let operations = ["+", "-", "*", "/"];
+
+    if(operations.includes(lastInput)) {
+      return;
+    }
+
+    this.currentEq += operation;
+    this.calcEq();
+  }
+
+  valueChange(){
+    let operations = ["+", "-", "*", "/"];
+
+    if(operations.includes(this.currentEq) || this.currentEq == "") {
+      return;
+    }
+
+    if(this.currentEq[0] == "-"){
+      this.currentEq = this.currentEq.slice(1);
+    } else {
+      this.currentEq = "-" + this.currentEq;
+    }
+  }
+
+  backup() {
+    if (this.currentEq !="" ) {
+      this.currentEq=this.currentEq.slice(0, this.currentEq.length-1)
+    }
+  }
+ 
+  clear() {
+    this.result = '';
+    this.currentEq = '';
+  }
+
+  calcEq() {
+    let problem = this.currentEq;
+
+    let lastKey = problem[problem.length - 1];
+    let operations = ["+", "-", "*", "/"];
+ 
+    if (lastKey === '.' || operations.includes(lastKey)) {
+      problem=problem.slice(0,problem.length - 1);
+    }
+ 
+    this.result = eval(problem);
+    console.log(this.result);
+  }
+
+  solveEq(){
+    this.calcEq();
+    this.currentEq = this.result;
   }
 
 }
